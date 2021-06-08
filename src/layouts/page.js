40d9934928e1f3a8c27ecda3456ee9bfa20a0017
Stackbit Code Editor <projects@stackbit.com>
2021-06-08 06:21:02 +0000

    page.js: changed

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -24,7 +24,7 @@ export default class Page extends React.Component {
                             {subtitle && <div className="post-subtitle">{htmlToReact(subtitle)}</div>}
                         </header>
                         {image && (
-                            <div className="post-image">
+                            <div className="post-avatar">
                                 <img src={withPrefix(image)} alt={imageAlt} />
                                 <p className="avatar-name">Jhonny Alberto</p>
                                 <p className="avatar-skill">React | Node JS</p>                                
