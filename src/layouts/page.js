72d49c579dd5b7d5d87bd311c7520c800592121e
Stackbit Code Editor <projects@stackbit.com>
2021-06-08 05:51:41 +0000

    page.js: changed

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -32,9 +32,8 @@ export default class Page extends React.Component {
                         )}
                         <div className="post-content inner-sm">
                             <p className="post-contact">contact me on <a href="#">upwork</a></p>
-                            <p></p>
                         </div>
-                        {/* {markdownContent && <div className="post-content inner-sm">{markdownify(markdownContent)}</div>} */}
+                        {markdownContent && <div className="post-content inner-sm">{markdownify(markdownContent)}</div>}
                     </article>
                 </div>
             </Layout>
